01d235bf67309ad77041661269aeb8b2acd5d4c6
RedBlizard <grote2207@gmail.com>
2026-05-23 23:27:28 +0200

    commit new wallpaper tool

---
+++ .config/hypr-welcome/scripts/waytrogen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Step 1: Start waytrogen
+waytrogen &
+
+# Store the PID of waytrogen
+waytrogen_pid=$!
+
+# Step 2: Kill yad (hypr-welcome)
+pkill -f yad
+
+# Step 3: Check if waytrogen is closed
+while kill -0 $waytrogen_pid 2>/dev/null; do
+    sleep 1
+done
+
+# Step 4: Restart hypr-welcome
+hypr-welcome &
