1cc01f34367f04d4c2a47de54a00091ed2fc408f
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2026-06-06 18:32:27 +0200

    Delete .config/hypr-welcome directory

--- .config/hypr-welcome/scripts/no_updates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yad --info \
-    --title="Hyprland" \
-    --text="All is already up-to-date! 🚀" \
-    --image="$HOME/.config/hypr-welcome/assets/logo.png" \
-    --no-buttons \
-    --timeout=4
